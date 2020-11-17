ee89df3c723c0725a78ac9688d6a02232160bb39
wujc <15755081023@163.com>
2020-11-17 09:22:35 +0800

    feat: class

---
+++ src/tsapi/genericity.ts
@@ -0,0 +1,15 @@
+function identity<T> (arg: T): T {
+  return arg
+}
+
+let myIdentity: <U>(aa: U) => U = identity
+
+console.log(myIdentity('123').length)
+
+let myIdentity1: {<T>(arg: T): T} = identity
+
+function getProperty<T, U extends keyof T> (obj:T, key:U) {
+  return obj[key]
+}
+
+getProperty({ a: 123, b: 123 }, 'a')
